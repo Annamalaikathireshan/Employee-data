de7d9820457aba4e1e4b70dac492be9713a350c9
SarmilaRV <sivasarmi2001@mail.com>
2023-12-24 01:43:50 +0530

    Created the  table for emp data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import employeeData from '../assets/employeeData.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'employee-datax';
 }
+const empData = employeeData;
+console.log(empData);
